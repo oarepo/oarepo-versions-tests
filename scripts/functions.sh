d840333b0123a518f24ad014278d5448bc71b535
Tom Hlava <hlava@cesnet.cz>
2023-12-12 16:07:21 +0100

    api_get_trigrun changed; tag added

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -23,19 +23,17 @@ function api_trig(){
 
 function api_get_trigrun(){
   local url=${1:?"api_call: url undefined"} ; shift
-  local event="${1:-workflow_dispatch}" ; shift
-  local concl="$1" ; shift
+  local concl="${1:-}" ; shift
   local idx="${1:-0}" ; shift
-  local status="${concl:+"&status=$concl"}"
-  local cond='.path==".github/workflows/manual.yaml"'
-  local fullurl="$url/actions/runs?branch=$BR&actor=$BOT&event=$event$status"
+  local cond='(.event=="push" or .event=="workflow_dispatch") and (.referenced_workflows[0].ref|test("^refs/(tags|heads/'$BR')"))'
+  local fullurl="$url/actions/runs?status=$concl"
   api_call "$fullurl" "GET" | jq -r "
     [
       .workflow_runs[]
       | select($cond)
     ]
     | sort_by(.run_started_at) | reverse [$idx]
-    | [.run_started_at,.conclusion]
+    | [.run_started_at,.conclusion,.referenced_workflows[0].ref]
     |@tsv
   "
 }
